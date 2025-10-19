1607621906a895b20a495d957639778a7bdc57b0
bluefrog1413 <162390173+bluefrog1413@users.noreply.github.com>
2025-10-19 17:07:05 +0900

    fix: 입력 문자열의 \\n을 실제 개행 문자로 변환하도록 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,5 +6,8 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+
+        // \\n을 실제 개행 문자로 변환
+        input = input.replace("\\n", "\n");
     }
 }
